bed37f5e566c2808f9e13c3f0b462fdd3cd72225
Mobile Patagonia <mobilepatagonia@gmail.com>
2020-10-07 12:51:45 -0300

    Swift UI Started

--- canvas-1/ContentView.swift
+++ canvas-1/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.gray)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(1)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
             .padding()
+            
+            Spacer()
+        }
+            
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .environment(\.sizeCategory, .extraSmall)
     }
 }
