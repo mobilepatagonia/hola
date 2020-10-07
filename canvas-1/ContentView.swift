3f0de792f6d2f77928eeecb1dfa5a9f94554e321
Javier Fuchs <javier.fuchs@gmail.com>
2020-10-07 11:36:13 -0300

    Initial Commit

---
+++ canvas-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  canvas-1
+//
+//  Created by Javier Fuchs on 07/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ canvas-1/canvas_1App.swift
@@ -0,0 +1,17 @@
+//
+//  canvas_1App.swift
+//  canvas-1
+//
+//  Created by Javier Fuchs on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct canvas_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
